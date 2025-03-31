4f786461e6760013c06e652141431de8dd7b929e
MaheryAndrih1 <maheryandrianaivo1@gmail.com>
2025-04-01 02:38:24 +0300

    added frontend to allowed origins

--- techshelf/settings.py
+++ techshelf/settings.py
@@ -47,7 +47,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",     
-    "http://127.0.0.1:5173",    
+    "http://127.0.0.1:5173", 
+    "https://techshelf-frontend.pages.dev"   
 ]
 CORS_ALLOW_CREDENTIALS = True
 
